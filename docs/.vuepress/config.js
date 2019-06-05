248100980b759267ecb651f245cfa501856e00ff
amodinho <u14284783@tuks.co.za>
2019-06-05 12:07:38 +0200

    changed the relative path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,7 @@
 module.exports = {
     title: 'GOA Documentation',
-    description: 'A documentation site to provide help for using GOA.'
+    description: 'A documentation site to provide help for using GOA.',
+    postcss: {
+        plugins: [require('tailwindcss')('../../tailwind.js'), require('autoprefixer')],
+    }
 }
