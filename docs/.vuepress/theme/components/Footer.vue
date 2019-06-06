e4c67a9f1db2e7c9732c7fbb0019c9a20252d2ea
amodinho <u14284783@tuks.co.za>
2019-06-06 12:02:35 +0200

    aDDED THE CSS

--- docs/.vuepress/theme/components/Footer.vue
+++ docs/.vuepress/theme/components/Footer.vue
@@ -39,3 +39,15 @@ export default{
     name: 'Footer'
 }
 </script>
+
+<style>
+footer {
+    clip-path: polygon(59% 21%, 100% 1%, 100% 100%, 0 100%, 0 49%);
+    height: 600px;
+    /*transform: matrix(0.98, -0.2, 0.21, 0.98,0,0);*/
+    
+}
+.Links__Container, .Copyright__Container{
+    left:35vw;
+}
+</style>
