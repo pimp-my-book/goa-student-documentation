f41ca3a16d950ab383f442d1f03d0a505636a822
amodinho <u14284783@tuks.co.za>
2019-06-10 13:02:00 +0200

    Added the background image to it

--- docs/.vuepress/theme/Layouts/HomeLayout.vue
+++ docs/.vuepress/theme/Layouts/HomeLayout.vue
@@ -1,7 +1,8 @@
 <template>
     <header
+    class="bg-green-graident h-250"
     >
-    
+
     </header>
 </template>
 <script>
@@ -9,3 +10,11 @@ export default {
     name: 'HomeLayout'
 }
 </script>
+
+<style >
+header{
+    background: linear-gradient(179.9deg, #A0FFAB 23.23%, #94B5E3 99.68%);
+}
+
+
+</style>
