c60d1abc5f85bdb01abb9c328f9f2ffd8bb5a76a
amodinho <u14284783@tuks.co.za>
2019-06-13 14:05:32 +0200

    Added display x large'

---
+++ src/components/typography/DisplayXLarge.vue
@@ -0,0 +1,11 @@
+<template>
+    <p class="text-5xl font-display">{{text}}</p>
+</template>
+<script>
+export default {
+    name:'DisplayXLarge',
+    props: {
+        text: String
+    }
+}
+</script>
