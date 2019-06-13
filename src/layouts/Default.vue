860c274ea0544993cadf2edfffb1d51130e25ed7
amodinho <u14284783@tuks.co.za>
2019-06-13 15:05:16 +0200

    Trying to get this stupid thing to work

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -25,9 +25,7 @@ query {
 
 <style>
 
-@tailwind preflight;
-@tailwind components; 
-@tailwind utilities;
+
 
 body {
   font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
