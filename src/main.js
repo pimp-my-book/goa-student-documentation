3f481fc47d98f5455e2e19c4c0f2850cdf1c485a
amodinho <u14284783@tuks.co.za>
2019-06-18 13:02:51 +0200

    Added font

--- src/main.js
+++ src/main.js
@@ -6,4 +6,10 @@ import DefaultLayout from '~/layouts/Default.vue'
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+
+  //Setting Up fonts
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Paytone+One|Poppins'
+  })
 }
