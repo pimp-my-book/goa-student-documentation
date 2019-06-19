4bf35fe98fcd291bc115f183b8ee6ede8f08ae2d
amodinho <u14284783@tuks.co.za>
2019-06-19 12:58:32 +0200

    Added the necessary styling

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,25 +1,42 @@
 <template>
   <Layout>
-   
+   <header
+    class="bg-green-graident h-250 text-center"
+    >
+    <DisplayXLarge
+    text="How can we help you?"
+    class="text-white p-5"
+    />
+    <FeatureText
+    text="We have made articles for you to follow if you get stuck or do not understand how to do something. "
+    />
+</header>
    
   </Layout>
 </template>
 
 <script>
-import NavigationBar from '~/components/NavigationBar.vue'
+import FeatureText from '~/components/typography/FeatureText.vue'
+import DisplayXLarge from '~/components/typography/FeatureText.vue'
 
 export default {
   metaInfo: {
     title: 'Hello, world!'
   },
   components:{
-    NavigationBar
+    DisplayXLarge,
+    FeatureText
   }
 }
 </script>
 
 <style>
-.home-links a {
-  margin-right: 1rem;
+header{
+    background: linear-gradient(179.9deg, #A0FFAB 23.23%, #94B5E3 99.68%);
+}
+.Card__Grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 10px;
 }
 </style>
