f0afb716eed9a0ec8de40ff162b37f9bb9f49382
amodinho <u14284783@tuks.co.za>
2019-06-20 15:13:35 +0200

    Done with templete test

--- src/templates/Article.vue
+++ src/templates/Article.vue
@@ -1,8 +1,19 @@
 <template>
-    
+    <div>
+        <span>{{$page.doc.title}}</span>
+        <span v-text="$page.doc.content"/>
+    </div>
 </template>
 <script>
 export default {
     name: 'Article'
 }
 </script>
+<page-query>
+query DocPost ($path: String!){
+  doc: article (path: $path){
+    title
+    content
+  }
+}
+</page-query>
