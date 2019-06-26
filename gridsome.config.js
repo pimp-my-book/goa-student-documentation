895c7cfc9a58e52ef039bd270d18ae58aca290eb
amodinho <u14284783@tuks.co.za>
2019-06-26 11:26:45 +0200

    Added a chainwebpack func.

--- gridsome.config.js
+++ gridsome.config.js
@@ -24,6 +24,15 @@ module.exports = {
       }
     }
   ],
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+  }
+  ,
+
   transformers: {
     remark:{
       externalLinksTarget: '_blank',
