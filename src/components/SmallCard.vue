3a6a36ae8c7e8d498f6fd0208715d1ccbbc0b030
amodinho <u14284783@tuks.co.za>
2019-07-18 11:54:16 +0200

    removed css classes

--- src/components/SmallCard.vue
+++ src/components/SmallCard.vue
@@ -1,5 +1,5 @@
 <template>
-<div class= "shadow-lg x:w-4/5 x:h-full Card rounded-lg lg:mr-10">
+<div class= "shadow-lg x:w-4/5 x:h-full xl:h-500 Card rounded-lg lg:mr-10">
  
   <div>
    <DisplaySmall 
@@ -35,9 +35,4 @@ export default {
 }
 </script>
 
-<style>
-.Card{
-    width: 300px;
-    height: 150px;
-}
-</style>
+
