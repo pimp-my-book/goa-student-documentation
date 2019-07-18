21f918b95c2c646694317e3c9ac7d15e40be7f48
amodinho <u14284783@tuks.co.za>
2019-07-18 14:23:16 +0200

    added responsive text to the credits

--- src/components/typography/Credits.vue
+++ src/components/typography/Credits.vue
@@ -1,5 +1,5 @@
 <template>
-    <p class="text-sm font-body text-white  pt-5">
+    <p class="text-sm font-body text-white xl:text-2xl  pt-5">
      Made with ❤️ in <img src="https://s3.amazonaws.com/goa-assets/Icons/flag-for-south-africa_1f1ff-1f1e6.svg"/>
      & <img src="https://s3.amazonaws.com/goa-assets/Icons/flag-for-india_1f1ee-1f1f3.svg"/>
     </p>
